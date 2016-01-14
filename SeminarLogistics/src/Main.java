4f9ae5be3c5eacd6bf4480256d2c154f958f063d
Philip Moll <philipmoll.0@gmail.com>
2016-01-14 11:03:17 +0100

    test329084

--- SeminarLogistics/src/Main.java
+++ SeminarLogistics/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args){
 		System.out.print("Philip is een zieke henk.");
 		System.out.print("Floor is een zieke henk.");
+		System.out.print("Robin is een zieke henk.");
 		
 	}	
 }
