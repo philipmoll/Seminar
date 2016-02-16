d7515d2c32c6726e9b2911d223f992e10922a498
floorwolfhagen <floorwolfhagen@gmail.com>
2016-02-16 13:54:22 +0100

    bla bla met errors

--- SeminarLogistics/src/Track.java
+++ SeminarLogistics/src/Track.java
@@ -287,6 +287,16 @@ public class Track {
 			}
 		}
 	}
+	public void removeBusyTimeMove(Activity activity) throws IOException{
+		for(int i = activity.getPlannedTimeInteger()+activity.getDurationInteger()-Main.moveduration; i<activity.getPlannedTimeInteger()+activity.getDurationInteger(); i++){
+			if(busytime[i] != null){
+				busytime[i] = null;
+			}
+			else{
+				throw new IOException("Cannot remove busytime if it does not exist.");
+			}
+		}
+	}
 
 	public boolean checkFeasibility(Activity activity, int timetobechecked, int checkmovetime){
 
