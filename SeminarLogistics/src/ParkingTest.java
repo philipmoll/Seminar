c45163685c0e215dd76ff7ac32b64da6d06a9d5f
Friso <frisotigchelaar@gmail.com>
2016-02-17 12:39:15 +0100

    abc

--- SeminarLogistics/src/ParkingTest.java
+++ SeminarLogistics/src/ParkingTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,6 +15,16 @@ public class ParkingTest {
 	private Track track2;
 	private Track track3;
 	
+	private Train train1;
+	private Train train2;
+	private Train train3;
+	private Train train4;
+	
+	private Composition composition1;
+	private Composition composition2;
+	private Composition composition3;
+	private Composition composition4;
+	
 	private Event event1;
 	private Event event2;
 	private Event event3;
@@ -23,12 +36,29 @@ public class ParkingTest {
 	
 	private FinalBlock block1;
 	private FinalBlock block2;
-	private FinalBlock block3;
-	private FinalBlock block4;
+	
+	
 
 	@Before
 	public void setUp() {
-		block1 = new FinalBlock
+		
+		track1 = new Track("track1", length, 1, 0, 0, 0, 0, backwardlength);
+		track2 = new Track("track2", length, 1, 0, 0, 0, 0, backwardlength);
+		track3 = new Track("track3", length, 1, 0, 0, 0, 0, backwardlength);
+		
+		int train_ID, int type, int carriages, int length
+		train1 = new Train(01, 1, 4, length);
+		train2 = new Train();
+		train3 = new Train();
+		train4 = new Train();
+		
+		composition1 = new Composition(new ArrayList(){{add(train1);add(train2);}}, arrivaltime, -1);
+		composition2 = new Composition(new ArrayList(){{add(train3);add(train4);}}, arrivaltime, -1);
+		composition3 = new Composition(new ArrayList(){{add(train1);add(train2);}}, -1, departuretime);
+		composition4 = new Composition(new ArrayList(){{add(train3);add(train4);}}, -1, departuretime);
+		
+			
+		block1 = new FinalBlock(null, 0, 0, block1, block1, 0, 0, 0, 0);
 	}
 
 	@Test
