3632c7480860297291a7ed198098a38b397af9d6
Friso <frisotigchelaar@gmail.com>
2016-02-17 11:17:15 +0100

    trackie

---
+++ SeminarLogistics/src/ParkingTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class ParkingTest {
+
+	//tracks, events, blocks
+	private Track track1;
+	private Track track2;
+	private Track track3;
+	
+	private Event event1;
+	private Event event2;
+	private Event event3;
+	private Event event4;
+	private Event event5;
+	private Event event6;
+	private Event event7;
+	private Event event8;
+	
+	private FinalBlock block1;
+	private FinalBlock block2;
+	private FinalBlock block3;
+	private FinalBlock block4;
+
+	@Before
+	public void setUp() {
+		block1 = new FinalBlock
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
